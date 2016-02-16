3441afd2cfc3d1176c17f8f5f7fde15e65edf338
mghadley <matthew.g.hadley@gmail.com>
2016-02-15 20:04:40 -0700

    fixing gemfile because openshift is annoying

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   authenticated do
     root to: 'pages#home', as: :authenticated_root
   end
+  
   root to: 'pages#index'
   resources :orders
   resources :measurements
