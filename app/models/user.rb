0d5d29515f0345dc2c78b564a490f474ae4626e9
mghadley <matthew.g.hadley@gmail.com>
2016-02-15 21:48:21 -0700

    added titleize

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 	has_many :orders
 	has_one :measurement
+	before_save { self.name = name.titleize }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
