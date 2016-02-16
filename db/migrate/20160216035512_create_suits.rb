827ae1ae3bbcc6f6be514515dccf507653848201
mghadley <matthew.g.hadley@gmail.com>
2016-02-15 22:33:25 -0700

    working on order form

---
+++ db/migrate/20160216035512_create_suits.rb
@@ -0,0 +1,26 @@
+class CreateSuits < ActiveRecord::Migration
+  def change
+    create_table :suits do |t|
+
+    	t.float :shoulders
+    	t.float :sleeve
+    	t.float :chest
+    	t.float :stomach
+    	t.float :jacket_length
+    	t.float :front
+    	t.float :back
+    	t.float :waist
+    	t.float :hips
+    	t.float :crotch
+    	t.float :pants_length
+    	t.float :thigh
+    	t.float :knee
+    	t.float :ankle
+
+    	t.references :material, index: true, foreign_key: true
+   		t.belongs_to :order, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
